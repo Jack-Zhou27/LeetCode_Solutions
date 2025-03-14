c956559ef78532e2843bb90cbe106dafcb04d77a
Jack-Zhou27 <70481678+Jack-Zhou27@users.noreply.github.com>
2025-03-14 16:32:43 -0400

    Create 0700_SearchInBST.py

---
+++ easy/0700_SearchInBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        
+        if val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+
+        
